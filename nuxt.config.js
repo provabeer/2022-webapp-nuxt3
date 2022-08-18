23931d58c2b7a48b9c66398d7f0c6a14be78a67b
flavionn <flavio8163@gmail.com>
2022-08-17 21:36:03 -0300

    create env file

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,13 +1,20 @@
 import { defineNuxtConfig } from 'nuxt'
 
 export default defineNuxtConfig({
-  buildModules: [
-    "@pinia/nuxt",
-  ],
-  modules: [
-    "@unocss/nuxt",
-  ],
+  runtimeConfig: {
+    public: {
+      firebaseApiKey: process.env.FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      firebaseDatabaseURL: process.env.FIREBASE_DATABASE_URL,
+      firebaseProjectID: process.env.FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSendID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppID: process.env.FIREBASE_APP_ID,
+    },
+  },
+  buildModules: ['@pinia/nuxt'],
+  modules: ['@unocss/nuxt'],
   experimental: {
     viteMode: true,
-  }
+  },
 })
--- plugins/firebase.client.js
+++ plugins/firebase.client.js
@@ -4,20 +4,22 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 export default defineNuxtPlugin(() => {
+  const config = useRuntimeConfig()
+
   const firebaseConfig = {
-    apiKey: 'AIzaSyDJdFHLH_jGQFcBN2hS47NvXgc1tWKFUd4',
-    authDomain: 'provabeer.firebaseapp.com',
-    databaseURL: 'https://provabeer.firebaseio.com',
-    projectId: 'provabeer',
-    storageBucket: 'provabeer.appspot.com',
-    messagingSenderId: '890053780568',
-    appId: '1:890053780568:web:5d326a6dd553710062d62d',
+    apiKey: config.firebaseApiKey,
+    authDomain: config.firebaseAuthDomain,
+    databaseURL: config.firebaseDatabaseURL,
+    projectId: config.firebaseProjectID,
+    storageBucket: config.firebaseStorageBucket,
+    messagingSenderId: config.firebaseMessagingSendID,
+    appId: config.firebaseAppID,
   }
-  
+
   const app = initializeApp(firebaseConfig)
   const auth = getAuth(app)
   const db = getFirestore(app)
-  
+
   return {
     provide: {
       firebaseApp: app,
